3c556f75ddd5e83638c6ca04d1b2caa2a1a454fb
roosboos <john.4@scale.net>
2025-03-31 16:12:05 -0400

    added refresh rate/FPS

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -9,5 +11,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
